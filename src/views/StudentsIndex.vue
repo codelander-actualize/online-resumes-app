e78ed1126241c810ad9f6e934c8dcc54d12a1bc4
candace <candace.marzec@gmail.com>
2019-03-11 13:30:13 -0700

    adds student index

---
+++ src/views/StudentsIndex.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="students-index">
+    <h1>All Students</h1>
+    <div v-for="student in students">
+      <h2>{{ student.full_name }}</h2>
+      <img v-bind:src="student.photo">
+      <p>{{ student.short_bio }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      students: []
+    };
+  },
+  created: function() {
+    // axios.get("/api/students").then(response => {
+    //   this.students = response.data;
+    // });
+  },
+  methods: {}
+};
+</script>
+
+
+Email
+Phone Number
+Short Bio
+LinkedIn URL
+Twitter Handle
+Personal Blog/Website URL
+Online Resume URL
+Github URL
+Photo
