9a73bc501dc8f59f51e8faad5d8ab1317ba132c5
candace <candace.marzec@gmail.com>
2019-03-14 09:50:29 -0700

    adds dummy data

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -2,8 +2,8 @@
   <div class="resume">
     <h1>Contact Info</h1>
 
-    <h2>{{ student.formatted.full_name }}</h2>
-    <img :src="student.photo_url" :alt="student.formatted.full_name">
+    <h2>{{ student.first_name }}</h2>
+    <img :src="student.photo_url" :alt="student.first_name">
     <p>First Name: {{ student.first_name }}</p>
     <p>Last Name: {{ student.last_name }}</p>
     <p>Email: {{ student.email }}</p>
@@ -31,7 +31,7 @@
       <p>Start Date: {{ education.start_date }}</p>
       <p>End Date: {{ education.end_date }}</p>
       <p>Degree: {{ education.degree }}</p>
-      <p>Institution: {{ education.university_name }}</p>
+      <!-- <p>Institution: {{ education.university_name }}</p> -->
       <p>{{ education.details }}</p>
     </div>
 
@@ -49,7 +49,7 @@
       <p>Name: {{ capstone.name }}</p>
       <p>{{ capstone.description }}</p>
       <p>Link: {{ capstone.url }}</p>
-      <img :src="capstone.image_url" :alt="capstone.name">
+      <!-- <img :src="capstone.image_url" :alt="capstone.name"> -->
     </div>
 
   </div>
@@ -63,15 +63,90 @@ import axios from "axios";
 export default {
   data: function() {
     return {
-      student: {}
+      student: {
+        id: 1,
+        first_name: "test",
+        last_name: "dummy",
+        email: "test@gmail.com",
+        phone_number: "123-4567",
+        short_bio: "sdfojsfpojpwofj",
+        linkedin_url: "linkedin/in/testdummy",
+        twitter_handle: "@test_dummy",
+        // personal_blog_website_url: "testdummy.com",
+        resume_url: "testdummy.com/resume",
+        github_url: "github.com/testdummy",
+        photo_url: "photo_test",
+        experiences: [
+          {
+            id: 1,
+            start_date: "1-1-1",
+            end_date: "12-12",
+            job_title: "dummy",
+            company_name: "test",
+            details: "working at test company"
+          },
+          {
+            id: 2,
+            start_date: "12-1-1",
+            end_date: "1-2-12",
+            job_title: "dummy1",
+            company_name: "test1",
+            details: "working as a dummy at test company"
+          }
+        ],
+        educations: [
+          {
+            id: 1,
+            start_date: "12-1-1",
+            end_date: "123-12",
+            degree: "BA",
+            // university_name: "ABC U",
+            details: "Degree in sales"
+          },
+          {
+            id: 2,
+            start_date: "112-1-1",
+            end_date: "2123-12",
+            degree: "BAS",
+            // university_name: "ABC JU",
+            details: "Degree in nothing"
+          }
+        ],
+        skills: [
+          {
+            id: 1,
+            skill: "ruby"
+          },
+          {
+            id: 2,
+            skill: "html"
+          }
+        ],
+        capstones: [
+          {
+            id: 1,
+            name: "cappy",
+            description: "a capstone",
+            url: "capstone.com"
+            // screenshot: "screenshot.com"
+          },
+          {
+            id: 2,
+            name: "cap 2",
+            description: "cap 2123-12",
+            url: "cap.com"
+            // screenshot: "screen.com"
+          }
+        ]
+      }
     };
   },
-  created: function() {
-    axios.get("/api/students/" + this.$route.params.id).then(response => {
-      console.log(response.data);
-      this.student = response.data;
-    });
-  },
+  // created: function() {
+  //   axios.get("/api/students/" + this.$route.params.id).then(response => {
+  //     console.log(response.data);
+  //     this.student = response.data;
+  //   });
+  // },
   methods: {}
 };
 </script>
